ae7092e72fc40f4040026c420fa2e8757d123fed
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-13 10:42:00 +0700

    update view

---
+++ Controller.java
@@ -0,0 +1,32 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private ChoiceBox<?> CB1;
+
+    @FXML
+    private ChoiceBox<?> CB2;
+
+    @FXML
+    private ChoiceBox<?> CB3;
+
+    @FXML
+    private Button button_submit;
+
+    @FXML
+    private Label clock;
+
+    @FXML
+    private Label label_result;
+
+    @FXML
+    void on_sumbit(ActionEvent event) {
+
+    }
+
+}
